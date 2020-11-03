e6da27db7a1ed7b747cfa4b813057eeb1a7561db
William Walsh <wwalsh@student.neumont.edu>
2020-11-03 01:57:47 -0700

    Created constants.js, populated by web scraper util scraper.js

---
+++ src/constants/constants.js
@@ -0,0 +1,61 @@
+const evidence = {
+    emfLevelFive: 'EMF Level 5',
+    ghostWriting: 'Ghost Writing',
+    fingerPrints: 'Fingerprints',
+    spiritBox: 'Spirit Box',
+    freezingTemperatures: 'Freezing Temperatures',
+    ghostOrb: 'Ghost Orb'
+};
+
+const ghosts = [
+    {
+        name: 'Banshee',
+        evidence: ['EMF Level 5', 'Fingerprints', 'Freezing Temperatures']
+    },
+    {
+        name: 'Demon',
+        evidence: ['Ghost Writing', 'Spirit Box', 'Freezing Temperatures']
+    },
+    {
+        name: 'Jinn',
+        evidence: ['EMF Level 5', 'Spirit Box', 'Ghost Orb']
+    },
+    {
+        name: 'Mare',
+        evidence: ['Spirit Box', 'Freezing Temperatures', 'Ghost Orb']
+    },
+    {
+        name: 'Oni',
+        evidence: ['EMF Level 5', 'Ghost Writing', 'Spirit Box']
+    },
+    {
+        name: 'Phantom',
+        evidence: ['EMF Level 5', 'Freezing Temperatures', 'Ghost Orb']
+    },
+    {
+        name: 'Poltergeist',
+        evidence: ['Fingerprints', 'Spirit Box', 'Ghost Orb']
+    },
+    {
+        name: 'Revenant',
+        evidence: ['EMF Level 5', 'Ghost Writing', 'Fingerprints']
+    },
+    {
+        name: 'Shade',
+        evidence: ['EMF Level 5', 'Ghost Writing', 'Ghost Orb']
+    },
+    {
+        name: 'Spirit',
+        evidence: ['Ghost Writing', 'Fingerprints', 'Spirit Box']
+    },
+    {
+        name: 'Wraith',
+        evidence: ['Fingerprints', 'Spirit Box', 'Freezing Temperatures']
+    },
+    {
+        name: 'Yurei',
+        evidence: ['Ghost Writing', 'Freezing Temperatures', 'Ghost Orb']
+    }
+];
+
+export { ghosts, evidence }
