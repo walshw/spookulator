4072786287664b62078131c22b40b70aa0d3ca42
William Walsh <wwalsh@student.neumont.edu>
2020-11-04 18:43:19 -0700

    Logic for disabling evidence, optimized possible remaining evidence calculation

--- src/components/EvidenceContainer.js
+++ src/components/EvidenceContainer.js
@@ -19,7 +19,8 @@ class EvidenceContainer extends Component {
             evidence: evidenceObject,
             possibleGhosts: ghostList,
             impossibleGhosts: [],
-            possibleRemainingEvidence: []
+            possibleRemainingEvidence: [],
+            impossibleRemainingEvidence: []
         }
     }
 
@@ -42,66 +43,51 @@ class EvidenceContainer extends Component {
 
         updatedEvidence[evidenceKey].isSelected = !updatedEvidence[evidenceKey].isSelected;
 
-        // logic to evalute evidence states should be done at the end of a toggle
-        // i.e Enabling previously disabled evidence, disabling impossible evidence
-
-        this.evaluateEvidenceStates(updatedEvidence);
-
-        // set state is async, probs do this AFTER evaluation of states
-        this.setState({ selectedEvidence: updatedEvidence });
+        this.calculateEvidenceStates(updatedEvidence);
 
         // have a clear button?
     }
 
-    evaluateEvidenceStates = (currentEvidence) => {
-        // I am having an issue with names
-        // we have evidence in the state which is an object with ALL evidence and is used for tracking what is clicked and what isnt
-        // THEN we have to get the evidence objects that are selected
-
-        const selectedEvidence = Object.keys(currentEvidence)
+    calculateEvidenceStates = (currentEvidence) => {
+        const selectedEvidenceNames = Object.keys(currentEvidence)
             .filter((eKey) => currentEvidence[eKey].isSelected)
             .map(eKey => currentEvidence[eKey].name);
 
-        // loop through the ghost list and collect all the names of ghosts that contain
-        // the selected evidence
-
-        // maybe add all the ghosts that dont match the evidence
-        // need to find the set difference ?
-        // FOCUS ON ALL THE POSSIBLE GHOSTS AND POSSIBLE REMAINING EVIDENCE FIRST
-
-        // this below has nothing to do with validating evidence states
         const possibleGhosts = [];
         const impossibleGhosts = [];
 
-        const possibleRemainingEvidenceSet = new Set();
+        const allPossibleEvidence = new Set();
 
         ghostList.forEach(ghost => {
-            if (selectedEvidence
+            if (selectedEvidenceNames
                 .every(evidence => ghost.evidence
                     .includes(evidence))) {
                 possibleGhosts.push(ghost);
 
-                const difference = ghost.evidence.filter(e => !selectedEvidence.includes(e));
-
-                difference.forEach(e => possibleRemainingEvidenceSet.add(e));
+                ghost.evidence.forEach(e => allPossibleEvidence.add(e))
 
             } else {
                 impossibleGhosts.push(ghost);
             }
         });
 
-        // do similar logic but now we check for evidence that is NOT in the possible evidence?
-        // maybe instead of adding it each time in the ELSE up above I can just find the set difference between all the
-        // possible evidence and all evidence
+        const possibleRemainingEvidenceNames = Array.from(allPossibleEvidence).filter(e => !selectedEvidenceNames.includes(e));
+        const impossibleRemainingEvidenceNames = Object.values(evidenceDictionary).filter(e => !possibleRemainingEvidenceNames.includes(e) && !selectedEvidenceNames.includes(e));
 
-        let possibleRemainingEvidence = [];
-
-        possibleRemainingEvidenceSet.forEach(evidenceName => possibleRemainingEvidence.push({ name: evidenceName }));
+        Object.keys(currentEvidence).forEach(eKey => {
+            if (impossibleRemainingEvidenceNames.includes(currentEvidence[eKey].name)) {
+                currentEvidence[eKey].isDisabled = true;
+            } else {
+                currentEvidence[eKey].isDisabled = false;
+            }
+        })
 
         this.setState({
             possibleGhosts: possibleGhosts,
             impossibleGhosts: impossibleGhosts,
-            possibleRemainingEvidence: possibleRemainingEvidence
+            possibleRemainingEvidence: possibleRemainingEvidenceNames.map(e => ({name: e})),
+            impossibleRemainingEvidence: impossibleRemainingEvidenceNames.map(e => ({name: e})),
+            selectedEvidence: currentEvidence
         });
     }
 
@@ -125,6 +111,11 @@ class EvidenceContainer extends Component {
                 <ListContainer
                     title="Impossible Ghosts"
                     contentList={this.state.impossibleGhosts}
+                />
+                <hr />
+                <ListContainer
+                    title="Impossible Evidence"
+                    contentList={this.state.impossibleRemainingEvidence}
                 />
             </div>
         )
